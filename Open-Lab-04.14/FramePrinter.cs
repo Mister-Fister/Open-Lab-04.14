fe253c2866c0f078bfa80eb4cf4c41700c7cf272
Matúš <matusskor@gmail.com>
2020-10-30 17:32:58 +0100

    zmeny

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +7,35 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            int a = 0;
+            for (int l = 0; l < strings.Length; l++)
+            {
+                if (strings[l].Length > a)
+                {
+                    a = strings[l].Length;
+                }
+            }
+            for (int i = 1; i < a + 4; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("*");
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.Write("* " + strings[i]);
+
+                for (int s = 0; s < a - strings[i].Length; s++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" *");
+            }
+            for (int i = 1; i < a + 4; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("*");
         }
     }
-}
+} 
+           
